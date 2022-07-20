13659e77454a1965b6e99a5197226f7a5fcaf2da
Your Namexenofonchiotopoulos@gmail.com <xenofonchiotopoulos@gmail.com>
2022-07-20 11:09:52 +0100

    Fixing rodeo algorithm

--- rodeo_test.py
+++ rodeo_test.py
@@ -14,6 +14,24 @@ from scipy.optimize import minimize
 import math
 import function_file as func
 from qulacs import gate
+from qulacs.circuit import QuantumCircuitOptimizer
+import sys
+sys.path.append(".")
+import h5py
+from ctypes import sizeof
+from cirq import QuantumState, quantum_state
+from qiskit import QuantumCircuit
+from qulacs import *
+from openfermion.ops import FermionOperator
+from openfermion import *
+import numpy as np
+from qulacs import ParametricQuantumCircuit
+import matplotlib.pyplot as plt
+from scipy.linalg import eigh
+import scipy as sci
+from scipy.optimize import minimize
+import math
+from qulacs.gate import *
 
 norb = 2
 #qubit number
@@ -32,12 +50,88 @@ for i in range(1,norb) :
 h0,hi =  func.Hed(U,e)
 h = h0 + hi
 hjw = jordan_wigner(h)
-#print(hjw) 
-H =  quantum_operator.create_quantum_operator_from_openfermion_text(f"{hjw}") 
+
+H =  quantum_operator.create_quantum_operator_from_openfermion_text(f"{hjw}")
+H2 =  quantum_operator.create_quantum_operator_from_openfermion_text(f"{hjw}"+f"\n(1e-8+0j) [Z{nq}]")
+cst = float(f"{hjw}".split()[0].split('+')[0].replace('(','').replace('[','').replace('\'',''))
+
 e,v = eigh(get_sparse_operator(hjw).todense())
 egs = e[0]
 
 
+#VQE 
+#-----------------------------------------------------
+
+def min_ansatz(nq,norb, d_theta):
+    circuit =  QuantumCircuit(nq)
+    count = 0
+    for i in range(nq):
+        circuit.add_RY_gate(i,d_theta[count])
+        count += 1
+    return circuit
+
+def min_ansatz_1(nq,norb, d_theta):
+    circuit =  QuantumCircuit(nq+1)
+    count = 0
+    for i in range(nq):
+        circuit.add_RY_gate(i,d_theta[count])
+        count += 1
+
+    return circuit
+
+def cost(d_theta):
+    state = QuantumState(nq) #Prepare |00000>
+    circuit = min_ansatz(nq, norb, d_theta) #Construct quantum circuit
+    circuit.update_quantum_state(state) #Operate quantum circuit on state
+    return H.get_expectation_value(state).real #Calculate expectation value of Hamiltonian
+
+def get_state(d_theta,norb):
+    nq = 2 * norb
+    state = QuantumState(nq+1) #Prepare |00000>
+    circuit = min_ansatz_1(nq, norb, d_theta) #Construct quantum circuit
+    circuit.update_quantum_state(state) #Operate quantum circuit on state
+    return state #Output the current state 
+
+dt = np.random.random(nq+ norb * (nq+nq-2))*1e-1
+circ = cost(dt)
+cost_history = []
+d_theta = np.random.random(nq+ norb * (nq+nq-2))*1e-2
+cost_history.append(cost(d_theta))
+method = "BFGS"
+options = {"disp": True, "maxiter": 50, "gtol": 1e-6}
+opt = minimize(cost, d_theta, method=method, callback=lambda x: cost_history.append(cost(x)))
+update_theta = opt.x
+q0 = get_state(update_theta, norb)
+print(cost_history)
+
+#print(q1.get_qubit_count())
+
+#Trotter step
+#-----------------------------------------------------
+
+def ctrotter(dt) :
+    qct = QuantumCircuit(nq+1)
+    for j in range(H.get_term_count()):
+        term = H.get_term(j)
+        pauli_index = term.get_index_list()
+        id_index = term.get_pauli_id_list()
+        ceof = term.get_coef()
+        if pauli_index == [] or id_index == []:
+            pauli_index == [0 for i in range(nq)]
+            id_index == [0 for i in range(nq)]
+
+#            continue
+
+        if ceof.imag != 0.0:
+            raise ValueError('Pauli gates coefficient is imaginary cannot preform real time evolution')
+
+        test_gate_ = gate.PauliRotation( pauli_index, id_index,-2*dt*ceof.real)
+
+        test_gate = gate.to_matrix_gate(test_gate_)
+        test_gate.add_control_qubit(nq,1)
+        qct.add_gate(test_gate)
+    return qct
+#-----------------------------------------------------
 #Function that creates the gaussian random values
 def create_gaussian_values(standard_deviation, cycles):
     rand_list = abs(np.random.normal(0.0, standard_deviation, cycles)).tolist()
@@ -51,76 +145,62 @@ max = 10
 #setting the maximum values a trotter evolution is allowed to use
 resolution = 0.1
 
-times = create_gaussian_values(0.1,4)
-print(times)
+times = create_gaussian_values(10,30)
+#times = [0.1*i for i in range(-30,30)]#create_gaussian_values(1,1)
+
+exact_value = []
 qs_list = []
 prob_list = []
 Energy = np.arange(min,max,spacing) #range of energies to testd
 
 for k in range(len(Energy)):
+
     probability_list = []
     qs = QuantumState(nq+1)
-    #qs.set_Haar_random_state() # random state preperation
+#    qs.set_Haar_random_state() # random state preperation
+    # X(1).update_quantum_state(qs)
+    # X(0).update_quantum_state(qs)
+    # X(norb).update_quantum_state(qs)
+    qs= q0.copy()
+#    X(norb+1).update_quantum_state(qs)
+
     qc = QuantumCircuit(nq+1)
-    qc.add_P0_gate(nq) # projection of ancilla qubit to the 0 state
-    for i in range(len(times)): 
-        qc.add_H_gate(nq)   # hadamard gate as seen in paper 
-        if(times[i] > resolution):
-            reps = np.arange(0,times[i],resolution) # breaking down the i'th element of times if it is greater the 0.1 for accurate trotter time evolution
-            for trot_reps in reps:
-                #getting hamiltionan terms to input into control gate
-                for j in range(H.get_term_count()):
-                    term = H.get_term(j)
-                    pauli_index = term.get_index_list()
-                    id_index = term.get_pauli_id_list()
-                    ceof = term.get_coef()
 
-                    #creating tests for imaginary values and allowing empty pauli indices for the first coeff
-                    if pauli_index == [] or id_index == []:
-                        continue
-                    if ceof.imag != 0.0:
-                        raise ValueError('Pauli gates coefficient is imaginary cannot preform real time evolution')
-                    
-                    #creating a pauli rotation gate using the hamiltonian information and adding a control to the ancilla qubit
-                    test_gate_ = gate.PauliRotation( pauli_index, id_index,-2*resolution*ceof.real)
-                    test_gate = gate.to_matrix_gate(test_gate_)
-                    test_gate.add_control_qubit(nq,0)
-                    qc.add_gate(test_gate)
-                #carrying out the trotter time evolution for the requierd steps
-                for k in range(int(trot_reps/resolution)):
-                    qc.update_quantum_state(qs)
+    P0(nq).update_quantum_state(qs) # projection of ancilla qubit to the 0 state
+    qs.normalize(qs.get_squared_norm())
 
-        else:
-            #getting hamiltionan terms to input into control gate
-            for j in range(H.get_term_count()):
-                term = H.get_term(j)
-                pauli_index = term.get_index_list()
-                id_index = term.get_pauli_id_list()
-                ceof = term.get_coef()
+    prob=1
 
-                #creating tests for imaginary values and allowing empty pauli indices for the first coeff
-                if pauli_index == [] or id_index == []:
-                    continue
-                if ceof.imag != 0.0:
-                    raise ValueError('Pauli gates coefficient is imaginary cannot preform real time evolution')
-                
-                #creating a pauli rotation gate using the hamiltonian information and adding a control to the ancilla qubit
-                test_gate_ = gate.PauliRotation( pauli_index, id_index,-2*resolution*ceof.real)
-                test_gate = gate.to_matrix_gate(test_gate_)
-                test_gate.add_control_qubit(nq,0)
-                qc.add_gate(test_gate)
-            #carrying out the trotter time evolution for the requierd steps
-            for k in range(int(times[i]/resolution)):
-                qc.update_quantum_state(qs)
-        
-        qc.add_U1_gate(nq, Energy[k] * times[i]) #phase gate that rotates the ancilla qubit by P(E*T) where T is random time
-        qc.add_H_gate(nq)
-        qc.update_quantum_state(qs) 
-        probability_list.append(qs.get_zero_probability(nq)) #storing the probability that the nq qubit is in the 0 state
-        
-    qs_list.append(qs)
-    prob_list.append(probability_list) 
+    for i in range(len(times)):
 
-W = np.arange(min,max,spacing)
-plt.plot(W,prob_list)
+        nstep = int(abs(times[i])/(resolution))+1
+        dt = times[i]/nstep
+
+        qct = ctrotter(dt)
+        gate.H(nq).update_quantum_state(qs)
+        for j in range(nstep) :
+            qct.update_quantum_state(qs)
+        U1(nq, Energy[k] * times[i]).update_quantum_state(qs)
+        gate.H(nq).update_quantum_state(qs)
+
+        prob = prob*(qs.get_zero_probability(nq))
+        P0(nq).update_quantum_state(qs)
+        qs.normalize(qs.get_squared_norm())
+    print(Energy[k], H2.get_expectation_value(qs).real,prob)
+    prob_list.append(prob)
+    exact_value.append(H2.get_expectation_value(qs).real)
+
+max1 = prob_list[0]    
+
+count = 0
+
+for i in range(0, len(prob_list)):  
+    #Compare elements of array with max    
+    if(prob_list[i] > max1): 
+        count += 1     
+        max1 = prob_list[i]; 
+print('These are the largest values')
+print(Energy[count],exact_value[count],max1)
+
+plt.plot(Energy,prob_list)
 plt.show()
